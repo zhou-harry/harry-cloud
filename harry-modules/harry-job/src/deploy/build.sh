3d99a7a81d655cdb0af6242f56560ad0a6ff60c1
zhouhong <zhou.hong404@hotmail.com>
2023-03-29 19:14:07 +0800

    docker部署

---
+++ harry-modules/harry-job/src/deploy/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#定义变量
+API_NAME="harry-modules-job"
+API_VERSION=""
+API_PORT=9203
+IMAGE_NAME=$API_NAME
+CONTAINER_NAME=$API_NAME-$API_VERSION
+
+#构建docker镜像
+docker build -t $IMAGE_NAME .
+#删除docker容器
+cid=$(docker ps -a -q -f NAME="$CONTAINER_NAME")
+if [ "$cid" != "" ]; then
+   docker rm -f $cid
+   docker rmi $IMAGE_NAME
+fi
+#启动docker容器
+docker run -d -p $API_PORT:$API_PORT --name $CONTAINER_NAME $IMAGE_NAME
